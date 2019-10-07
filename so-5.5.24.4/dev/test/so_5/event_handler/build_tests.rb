412d4e6173d543f69b7b64bc8e41b6fb836a114b
Takashi SASAKI <takashi316@gmail.com>
2019-10-07 23:17:28 +0900

    downloaded so-5.5.24.4.tar.gz

---
+++ so-5.5.24.4/dev/test/so_5/event_handler/build_tests.rb
@@ -0,0 +1,31 @@
+#!/usr/local/bin/ruby
+require 'mxx_ru/cpp'
+
+path = 'test/so_5/event_handler'
+
+MxxRu::Cpp::composite_target {
+
+	required_prj( "#{path}/subscribe_errors/prj.ut.rb" )
+	required_prj( "#{path}/ignore_exception/prj.ut.rb" )
+	required_prj( "#{path}/exception_reaction_inheritance/prj.ut.rb" )
+	required_prj( "#{path}/unknown_exception_evt_handler/prj.ut.rb" )
+	required_prj( "#{path}/unknown_exception_evt_start/prj.ut.rb" )
+	required_prj( "#{path}/subscribe_before_reg/prj.ut.rb" )
+	required_prj( "#{path}/drop_subscr_in_lambda_event_handler/prj.ut.rb" )
+	required_prj( "#{path}/drop_subscr_in_lambda_svc_handler/prj.ut.rb" )
+	required_prj( "#{path}/has_subscription_1/prj.ut.rb" )
+	required_prj( "#{path}/has_subscription_2/prj.ut.rb" )
+	required_prj( "#{path}/has_subscription_3/prj.ut.rb" )
+	required_prj( "#{path}/has_subscription_4/prj.ut.rb" )
+	required_prj( "#{path}/has_subscription_5/prj.ut.rb" )
+	required_prj( "#{path}/has_subscription_6/prj.ut.rb" )
+	required_prj( "#{path}/has_subscription_7/prj.ut.rb" )
+	required_prj( "#{path}/has_subscription_8/prj.ut.rb" )
+	required_prj( "#{path}/external_subscription/prj.ut.rb" )
+	required_prj( "#{path}/deadletter_handler_simple/prj.ut.rb" )
+	required_prj( "#{path}/deadletter_handler_unsubscribe/prj.ut.rb" )
+	required_prj( "#{path}/deadletter_handler_unsubscribe_all_states/prj.ut.rb" )
+	required_prj( "#{path}/deadletter_handler_svc_request/prj.ut.rb" )
+	required_prj( "#{path}/deadletter_handler_has_handler/prj.ut.rb" )
+	required_prj( "#{path}/deadletter_handler_formats/prj.ut.rb" )
+}
